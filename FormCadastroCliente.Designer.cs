20d879440aeb3ba63c543025ad47a2bb55a0703e
DesenvolvimentoVisionCar <47982205+VictorCFerreira@users.noreply.github.com>
2023-12-06 21:41:33 -0300

    novo form cadastro clientes

---
+++ FormCadastroCliente.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace RADProjetoFinal
+{
+    partial class FormCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.corretoraDataSet = new RADProjetoFinal.CorretoraDataSet();
+            this.clientesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clientesTableAdapter = new RADProjetoFinal.CorretoraDataSetTableAdapters.ClientesTableAdapter();
+            this.tableAdapterManager = new RADProjetoFinal.CorretoraDataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.corretoraDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // corretoraDataSet
+            // 
+            this.corretoraDataSet.DataSetName = "CorretoraDataSet";
+            this.corretoraDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clientesBindingSource
+            // 
+            this.clientesBindingSource.DataMember = "Clientes";
+            this.clientesBindingSource.DataSource = this.corretoraDataSet;
+            // 
+            // clientesTableAdapter
+            // 
+            this.clientesTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.ApolicesTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ClientesTableAdapter = this.clientesTableAdapter;
+            this.tableAdapterManager.MarcasTableAdapter = null;
+            this.tableAdapterManager.ModelosTableAdapter = null;
+            this.tableAdapterManager.TabelaFIPETableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = RADProjetoFinal.CorretoraDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // FormCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 465);
+            this.Name = "FormCadastroCliente";
+            this.Text = "FormCadastroCliente";
+            this.Load += new System.EventHandler(this.FormCadastroCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.corretoraDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CorretoraDataSet corretoraDataSet;
+        private System.Windows.Forms.BindingSource clientesBindingSource;
+        private CorretoraDataSetTableAdapters.ClientesTableAdapter clientesTableAdapter;
+        private CorretoraDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+    }
+}
---
+++ FormCadastroCliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RADProjetoFinal
+{
+    public partial class FormCadastroCliente : Form
+    {
+        public FormCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void clientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.clientesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.corretoraDataSet);
+
+        }
+
+        private void FormCadastroCliente_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'corretoraDataSet.Clientes'. Você pode movê-la ou removê-la conforme necessário.
+            this.clientesTableAdapter.Fill(this.corretoraDataSet.Clientes);
+
+        }
+    }
+}
